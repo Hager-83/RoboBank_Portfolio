6b3f0e8f99f37f6284ea7303fbb59e51640f6ab8
Hager-83 <hagershohieb0@gmail.com>
2025-10-03 23:59:21 +0300

    Remove build

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Graduation_Project/Projects_final/RoboBank _Portfolio/googletest
-# Build directory: D:/Graduation_Project/Projects_final/RoboBank _Portfolio/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Graduation_Project/Projects_final/RoboBank _Portfolio/googletest/googlemock
-# Build directory: D:/Graduation_Project/Projects_final/RoboBank _Portfolio/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
